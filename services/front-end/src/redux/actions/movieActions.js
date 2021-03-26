b7baf989e07b36e9162afa7f40128f2b75a51a7c
Timothy Nguyen <quynhthoa1972@gmail.com>
2021-03-26 12:35:41 -0400

    Add some movie actions

--- services/front-end/src/redux/actions/movieActions.js
+++ services/front-end/src/redux/actions/movieActions.js
@@ -1,7 +1,16 @@
 import axios from 'axios';
-import { SEARCH_MOVIE, FETCH_MOVIES, FETCH_MOVIE, LOADING } from './types';
+import { SEARCH_MOVIE, FETCH_MOVIES, FETCH_POPULAR_MOVIES, FETCH_MOVIE, LOADING } from './types';
+import { TMDB_URL, TMDB_API_KEY } from "../../constants/config";
 
-
-export const fetchMovies = text => dispatch => {
-    axios.get()
+export const fetchPopularMovies = page => dispatch => {
+    axios
+        .get(`${TMDB_URL}/movie/popular?api_key=${TMDB_API_KEY}&page=${page}`)
+        .then(response =>
+        dispatch({
+            type: FETCH_POPULAR_MOVIES,
+            payload: response.data
+        })
+        ).catch(err => console.log(err));
 }
+
+
