f72064b75313961f845683e8aefcfe673b77bc02
Hans <hansquiogue@gmail.com>
2021-04-07 00:57:35 -0400

    Add initial recommender service

---
+++ services/recommender/api/db.py
@@ -0,0 +1,3 @@
+# TODO: Connect with our database
+# Temporary non-persistent movie data
+movie_data = ['Shrek', 'Shrek 2', 'Shrek 3']
---
+++ services/recommender/api/recommender.py
@@ -0,0 +1,17 @@
+from random import randint
+
+from typing import List
+from fastapi import Header, APIRouter
+
+from api.models import RecommenderIn, RecommenderOut
+import api.db as db
+
+recommender = APIRouter()
+
+# TODO: Use actual recommendation system
+@recommender.post('/recommend', response_model=RecommenderOut, status_code = 200)
+async def recommend_movie(payload: RecommenderIn):
+    # Retrieves a 'recommended movie' for a user
+    selected_movie = db.movie_data[randint(0, len(db.movie_data)) - 1]
+    
+    return { 'movie': selected_movie }
