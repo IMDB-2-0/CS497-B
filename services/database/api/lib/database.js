4ed2ddbbaa4062d1b98827fb2f356bedc728921f
Hans <hansquiogue@gmail.com>
2021-04-12 15:50:06 -0400

    Update build

--- services/database/api/lib/database.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pool = void 0;
-var pg_1 = require("pg");
-exports.pool = new pg_1.Pool({
-    // TODO: Utilize env variables to hide connection and details
-    connectionString: 'postgresql://postgres:postgres@postgresdb:5432/postgres'
-});
