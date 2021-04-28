1bc5fb1c282095d0461e01895ae3714418ff5c8e
Timothy Nguyen <quynhthoa1972@gmail.com>
2021-04-27 20:29:50 -0400

    Clear out unnecessary code from authActions.js

--- services/dashboard/src/redux/actions/authActions.js
+++ services/dashboard/src/redux/actions/authActions.js
@@ -46,58 +46,6 @@ export const googleLogin =  (googleData, history) => async(dispatch) => {
     })
 };
 
-// Register User
-export const registerUser = (userData, history) => (dispatch) => {
-  axios
-    .post('/auth/register', userData)
-    .then(() => history.push('/login'))
-    .catch((err) => {
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data,
-      });
-    });
-};
-
-// Login - get user token
-export const loginUser = (userData, history) => (dispatch) => {
-  axios
-    .post('/auth/login', userData)
-    .then((res) => {
-      if (res.status === 400) {
-        // eslint-disable-next-line no-console
-        console.log(res.json());
-      }
-
-      const { token } = res.data;
-      // Set token to localStorage
-      localStorage.setItem('jwtToken', token);
-      // Set token to Auth header
-      setAuthToken(token);
-      // Decode token to get user data
-      const decoded = jwtDecode(token);
-      // Set current user
-      dispatch(setCurrentUser(decoded));
-      // Add possible message
-      message.success('Login Successful');
-      // Go to Home page
-      history.push('/');
-    })
-    .catch((err) => {
-      // eslint-disable-next-line no-console
-      console.log(err.response.data);
-      if (typeof err.response.data.emailnotfound !== 'undefined') {
-        message.error("Email doesn't exist");
-      } else if (typeof err.response.data.passwordincorrect !== 'undefined') {
-        message.error('Password is incorrect');
-      }
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data,
-      });
-    });
-};
-
 // Log user out
 export const logoutUser = () => (dispatch) => {
   // Remove token from local storage
