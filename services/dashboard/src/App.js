0b7e3bf538fa6ed8aa0107793a91ae10c55e510c
Timothy Nguyen <quynhthoa1972@gmail.com>
2021-04-27 20:07:19 -0400

    Added new package to properly do auth

--- services/dashboard/src/App.js
+++ services/dashboard/src/App.js
@@ -40,8 +40,6 @@ function App() {
           <main>
             <Switch>
               <Route path="/login" component={Login} />
-            </Switch>
-            <Switch>
               <>
                 <Container
                   style={{
--- services/dashboard/src/redux/reducers/errorReducer.js
+++ services/dashboard/src/redux/reducers/errorReducer.js
@@ -2,6 +2,7 @@ import { GET_ERRORS } from '../actions/types';
 
 const initialState = {};
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default function (state = initialState, action) {
   switch (action.type) {
     case GET_ERRORS:
