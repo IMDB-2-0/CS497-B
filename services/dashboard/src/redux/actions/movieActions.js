969eef4c4721a8f3b410c74e0796b154d303d39e
Hans <hansquiogue@gmail.com>
2021-05-05 04:51:45 -0400

    Update to match MovieLens genres

--- services/dashboard/src/redux/actions/movieActions.js
+++ services/dashboard/src/redux/actions/movieActions.js
@@ -77,7 +77,7 @@ export const deleteLiked = (id, movieID) => {
                 message.success(res.data.message);
             })
             .catch(error => {
-                console.log('Disliked', error); 
+                message.error('An error occurred. Please try again.');
             });
             
     }
@@ -93,14 +93,14 @@ export const addLiked = (userID, movie, rating) => {
         80: 'Crime',
         99: 'Documentary',
         18: 'Drama',
-        10751: 'Family',
+        10751: 'Children',
         14: 'Fantasy',
         36: 'History',
         27: 'Horror',
-        10402: 'Music',
+        10402: 'Musical',
         9648: 'Mystery',
         10749: 'Romance',
-        878: 'Science Fiction',
+        878: 'Sci-Fi',
         10770: 'TV Movie',
         53: 'Thriller',
         10752: 'War',
@@ -119,6 +119,6 @@ export const addLiked = (userID, movie, rating) => {
             message.success(res.data.message);
         })
         .catch(error => {
-            console.log('Add', error); 
+            message.error('An error occurred. Please try again.');
         });
 }
