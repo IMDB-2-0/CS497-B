51a539e03ea903c2659033415d81239eaf72d627
Hans <hansquiogue@gmail.com>
2021-05-06 02:20:36 -0400

    Implement best fitted model with recommender (#7)

--- services/recommender/api/recommender.py
+++ services/recommender/api/recommender.py
@@ -1,7 +1,8 @@
-from pyspark.ml.recommendation import ALSModel
 from pyspark.sql import Row
+from pyspark.sql.functions import explode, col
+from pyspark.ml.recommendation import ALSModel
 
-from api.sparkSession import spark, links, movies
+from api.sparkSession import spark, fitted_model, links, movies
 from api.models import RatingsIn
 import pandas as pd
 from typing import List
@@ -13,24 +14,31 @@ def get_alias(predictions, links, movies):
     m = movies.alias('m')
     return (r, l, m)
 
+
 def join_tables(predictions, links, movies):
     r, l, m = get_alias(predictions, links, movies)
     return r.join(l, r.movieid == l.movieid) \
             .join(m, m.movieid == l.movieid)
 
+
 async def recommender(user_ratings: RatingsIn):
-    # Loads fitted model
-    model = ALSModel.load('temp-fitted-model')
     # Converts HTTP response of user ratings to Spark DataFrame
     data = spark.createDataFrame(Row(**x) for x in user_ratings)
-    # Creates prediction for a user
-    predictions = model.transform(data.select(['userid', 'movieid']))
 
-    # Joined results based on prediction
-    results = join_tables(predictions, links, movies)
-    # Selects rows we need for response
-    results = results.orderBy('prediction', ascending = False) \
-                     .select(['r.movieid', 'm.title', 'l.imdbid', 'l.tmdbid'])
+    # Creates prediction for a user
+    predictions = fitted_model.recommendForUserSubset(data, 6)
     
-    # Returns DataFrame results as a list of objects
-    return results.rdd.map(lambda row: row.asDict()).collect()
+    # Predictions are not empty
+    if predictions.count() > 0:
+
+        predictions = predictions.withColumn('rec_exp', explode('recommendations')) \
+                            .select('userid', col('rec_exp.movieid'))
+
+        # Joined results based on prediction
+        results = join_tables(predictions, links, movies)
+
+        # Returns DataFrame results as a list of objects
+        return results.rdd.map(lambda row: row.asDict()).collect()
+    
+    else:
+        return {}
--- services/recommender/api/sparkSession.py
+++ services/recommender/api/sparkSession.py
@@ -1,12 +1,21 @@
 from pyspark.sql import SparkSession
+from pyspark.ml.recommendation import ALSModel
 
 spark = SparkSession.builder \
     .config('spark.driver.extraClassPath', 'postgresql-42.2.19.jar') \
+    .config('spark.driver.memory', '6g') \
+    .config('spark.executor.memory', '6g') \
+    .config('spark.ui.enabled', 'false') \
     .appName('Recommender') \
     .getOrCreate()
+    
 
 # TODO
 url = 'jdbc:postgresql://postgresdb:5432/postgres?user=postgres&password=postgres'
 
 links = spark.read.jdbc(url=url, table='links')
 movies = spark.read.jdbc(url=url, table='movies')
+
+# TODO: Update with 'als-fitted-model'. 
+# For now uses fitted model that includes Hans' ratings from database
+fitted_model = ALSModel.load('als-fitted-model-presentation-only') 
