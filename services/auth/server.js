42b165cc33f1ef5ec63551d2a05f02b27625d970
Timothy Nguyen <quynhthoa1972@gmail.com>
2021-05-07 18:12:48 -0400

    Removed more code from server.js for auth

--- services/auth/server.js
+++ services/auth/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = require('./routes');
 const cors = require('cors');
-// const errors = require('celebrate');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,7 +10,6 @@ const host = '0.0.0.0';
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-// app.use(errors());
 
 // Allows routes to be used
 app.use('/api/v1/auth', router);
